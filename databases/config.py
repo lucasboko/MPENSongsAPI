8ea57264924e0742d0eb95c9356a1c0655f8b284
Lucas <lucas@clinicsense.com>
2026-02-19 14:41:01 -0500

    update db connection string

--- databases/config.py
+++ databases/config.py
@@ -1,11 +1,12 @@
 import pymongo
 from pymongo import AsyncMongoClient
 from dotenv import dotenv_values
+import os
 
 config = dotenv_values(".env")
 
 client = AsyncMongoClient(
-    "mongodb+srv://lucasboko_db_user:a1IOfOm5FVvEpaAQ@cluster0.6bjvmbp.mongodb.net/?appName=Cluster0",
+    "mongodb+srv://" + os.environ.get('DB_USER') + ":" + os.environ.get('DB_PASSWORD') + "@cluster0.6bjvmbp.mongodb.net/?appName=Cluster0",
     server_api=pymongo.server_api.ServerApi(version="1", strict=True,deprecation_errors=True)
     )
 db = client.get_database("mpen_lyrics")
